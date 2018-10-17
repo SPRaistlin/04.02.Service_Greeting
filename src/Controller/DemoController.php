1ec654186eaf8de5960dc95893309cddbeb958c9
marianoSP <mariano.sanchez@ebolution.com>
2018-10-17 13:53:02 +0200

    Tema 4 Ejercicio 2

---
+++ src/Controller/DemoController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use App\Services\Greeting;
+
+class DemoController extends AbstractController
+{
+    /**
+     * @Route("/demo/{name}", name="demo")
+     */
+    public function index($name, Greeting $greeting)
+    {
+        dump($name);
+        $greet = $greeting->greet($name);
+
+        return new Response($greet);
+
+        /*return $this->render('demo/index.html.twig', [
+            'controller_name' => 'DemoController',
+        ]);*/
+    }
+}
